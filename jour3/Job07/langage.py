abd7ff45d61647c381acad5cfcb6422477f41dc4
he-yu-cooper <he.yu-cooper@laplateforme.io>
2023-11-22 11:54:32 +0100

    def langage

---
+++ jour3/Job07/langage.py
@@ -0,0 +1,17 @@
+def check_language(langage):
+    if langage == 'JavaScript':
+        print("tu es un développeur web")
+    elif langage == 'python':
+        print("tu es un développeur IA")
+    elif langage == 'java':
+        print("tu es un développeur logiciel")
+    elif langage == 'reactjs':
+        print("tu es un developpeur mobile")
+    else:
+        print("un jour, je serai le meilleur développeur...")
+
+check_language('JavaScript')
+check_language('python')  
+check_language('java')  
+check_language('reactjs') 
+check_language('C++')  
