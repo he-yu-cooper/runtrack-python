fe746c9fc8e567efebd7ba19acad884843ff4d85
he-yu-cooper <he.yu-cooper@laplateforme.io>
2023-11-22 11:29:51 +0100

    def check number

---
+++ jour3/Job06/nombre.py
@@ -0,0 +1,11 @@
+def check_number(nombre):
+    if nombre > 0:
+        print("positif")
+    elif nombre < 0:
+        print("negatif")
+    else:
+        print("zero")
+
+check_number(5)
+check_number(-1)
+check_number(0)
