7acba1387d7532180707fc37b0e26e71aa5f5c76
he-yu-cooper <he.yu-cooper@laplateforme.io>
2023-11-23 11:51:32 +0100

    remove duplicates

---
+++ jour04/Job13/remove.py
@@ -0,0 +1,10 @@
+def remove_duplicates(L):
+    new_list = []
+    for i in L:
+        if i not in new_list:
+            new_list.append(i)
+    return new_list
+
+L = [10, 20, 30, 20, 10, 50, 60, 40, 80, 50, 40]
+print(remove_duplicates(L))
+
