3cc53c6c193b75072f28ad5fbe8763a0f51d0bf2
he-yu-cooper <he.yu-cooper@laplateforme.io>
2023-11-24 11:33:05 +0100

    n

--- jour05/Job04/n.py
+++ jour05/Job04/n.py
@@ -1,13 +1,18 @@
-def draw_diagonal(n):
-    # 打印图案的顶部边框
-    print(' ' + '_' * (n+2) + ' ')
+def draw_carpet_with_diagonal_and_frame(n):
+    
+    print("+" + "-" * (2 * n + 1) + "+")
 
-    # 打印图案的每一行
-    for i in range(n+1):
-        print('|' + '#' * i + ' ' * (n - i) + '#' * i + '|')
+    for i in range(n + 1):
 
-    # 打印图案的底部边框
-    print('|' + '_' * (n+2) + '|')
-
-# 调用函数
-draw_diagonal(10)
+        print("|", end="")
+        
+        for j in range(n + 1):
+            if i == n - j:
+    
+                print(' ', end=' ')
+            else:
+                print('#', end=' ')
+        
+        print("|")
+    print("+" + "-" * (2 * n + 1) + "+")
+draw_carpet_with_diagonal_and_frame(10)
