4e4fe6197e28f973a47a5f880a72f129444d4123
he-yu-cooper <he.yu-cooper@laplateforme.io>
2023-11-24 10:53:38 +0100

    input

---
+++ jour05/Job01/input().py
@@ -0,0 +1,2 @@
+name = input(" please entre your name: ")
+print("Hello, " + name + " !")
---
+++ jour05/Job01/withdef.py
@@ -0,0 +1,5 @@
+def say_hello():
+    name = input("Please entre your name：")
+    print(f"Hello，{name}！")
+
+say_hello()
